9715d53410628c63eaa289c836fa4e38e9a8ad0e
Dave Horton <daveh@beachdognet.com>
2025-02-25 20:48:25 -0500

    add some try/catch

--- session-wrapper.cpp
+++ session-wrapper.cpp
@@ -5,25 +5,36 @@ extern "C" {
 
 // Create a new Session instance
 void *create_session() {
-  //std::cout << "Creating a new session\n";
-  return new Session();
+  try {
+    return new Session();
+  } catch (const std::exception &e) {
+    std::cerr << "Session creation failed: " << e.what() << std::endl;
+    return nullptr;
+  } catch (...) {
+    std::cerr << "Session creation failed, reason unknown "<< std::endl;
+    return nullptr;
+  }
 }
 
 // Destroy an existing Session instance
 void destroy_session(void *session) {
-  //std::cout << "Destroying a session\n";
-  delete static_cast<Session *>(session);
+  if (session) {
+    delete static_cast<Session *>(session);
+  }
 }
 
 // Add data to the Session buffer
 void add_data_to_session(void *session, int isBinary, const char *data, size_t len) {
-  static_cast<Session *>(session)->addData(isBinary, data, len);
+  if (session) {
+    static_cast<Session *>(session)->addData(isBinary, data, len);
+  }
 }
 
 // Notify the Session that the connection is closed
 void notify_session_close(void *session) {
-  //std::cout << "Notifying session of close\n";
-  static_cast<Session *>(session)->notifyClose();
+  if (session) {
+    static_cast<Session *>(session)->notifyClose();
+  }
 }
 
 } // extern "C"
